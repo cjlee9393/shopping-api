138c1a7666ce3c730e57cb7fd00d2f17910cf56f
cjlee <cjlee9393@gmail.com>
2022-08-19 21:12:27 +0900

    initial commit to starter code

---
+++ migrations/sqls/20220815134142-test-up.sql
@@ -0,0 +1,2 @@
+/* Replace with your SQL commands */
+CREATE TABLE test (id SERIAL PRIMARY KEY, col1 VARCHAR(100), col2 VARCHAR(100));
---
+++ src/models/book.ts
@@ -0,0 +1,47 @@
+import client from '../database'
+
+export type Book {
+    id : Number;
+    title: String;
+    author: String;
+    total_pages: Number;
+    type: String;
+    summary: String;
+}
+
+export class studySessionStore {
+    async index(): Promise<Book[]> {
+        try{
+            const conn = await client.connect()
+            const sql = 'SELECT * FROM books'
+            const result = await conn.query(sql)
+            conn.release()
+            return result.rows
+        }catch(error: unknown){
+            throw new Error(`Error: ${error}`);
+        }
+    }
+    
+    async create(b: Book): Book {
+        try{
+            const conn = await client.connect()
+            const sql = 'INSERT INTO books (title, author, total_pages, type, summary) VALUES($1, $2, $3, $4, $5) RETURNING *'
+            const result = await conn.query(sql, [b.title, b.author, b.total_pages, b.type, b.summary])
+            const book = result.rows[0]
+            conn.release()
+            return book
+        }catch(error: unknown){
+            throw new Error(`Error: ${error}`);
+        }
+    }
+
+    async delete(id: string): Void {
+        try{
+            const conn = await client.connect()
+            const sql = 'DELETE FROM books WHERE id=($1)'
+            const result = await conn.query(sql, [id])
+        }catch(error: unknown){
+            throw new Error(`Error: ${error}`);
+        }
+    }
+}
